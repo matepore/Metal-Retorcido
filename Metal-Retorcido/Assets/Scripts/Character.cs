ce162eeaedb44f6c9250b338a55aca352f4663bc
matepore <31634675+matepore@users.noreply.github.com>
2020-04-12 20:32:56 -0300

    Creación del protagonista
    - Se agregaron varios sprites para futuros usos.
    - Se eligió uno para representar al protagonista o player.
    - Se usó ese gameobject y se le creó movimiento.
    -  Se modificó el protagonista para que la camara lo persiga.
    - Se agregó una imagen de fondo como ejemplo para ver el movimiento de la camara.

---
+++ Metal-Retorcido/Assets/Scripts/Character.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float speed;
+    public float rotationSpeed;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move(Vector3 direction)
+    {
+        rb.velocity = direction * speed;
+        rb.transform.up += direction * rotationSpeed;
+    }
+}
---
+++ Metal-Retorcido/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Character auto;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        auto = GetComponent<Character>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direccion;
+        direccion.x = Input.GetAxis("Horizontal");
+        direccion.y = Input.GetAxis("Vertical");
+        direccion.z = 0;
+
+        auto.Move(direccion);
+
+        //La camara persigue al personaje.
+        Camera.main.transform.position = this.transform.position + Vector3.back * 10;
+    }
+}
