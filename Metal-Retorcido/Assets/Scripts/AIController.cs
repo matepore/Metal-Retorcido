b2152c211fa0fb25754c86ddf69e962973db4316
matepore <31634675+matepore@users.noreply.github.com>
2020-04-22 01:22:24 -0300

    Consumibles y Enemigos al azar
    Se creó algunos consumibles para que cuando el auto colisione con ellos generen diferentes cosas. 2 de ellos son armas y 1 es un health pack. Ahora los enemigos eligen su oponente al azar. Se crearon 2 proyectiles nuevos.

--- Metal-Retorcido/Assets/Scripts/AIController.cs
+++ Metal-Retorcido/Assets/Scripts/AIController.cs
@@ -6,34 +6,64 @@ public class AIController : MonoBehaviour
 {
     Character auto;
     private GameObject enemigo;
+    int indice;
 
     // Start is called before the first frame update
     void Start()
     {
         auto = GetComponent<Character>();
-        enemigo = GameObject.Find("Heroe");
+
+        //Creo un array de autos
+        GameObject[] enemigos = GameObject.FindGameObjectsWithTag("Auto");
+
+        //Creo un indice random para que el personaje elija un enemigo al azar
+        indice = Random.Range(0, enemigos.Length);
+
+        //Igualo el objeto recibido a una variable
+        enemigo = enemigos[indice];
     }
 
     // Update is called once per frame
     void Update()
     {
+        //Pregunto si el enemigo elegido existe, esto sirve para que cuando tu enemigo muera no lo siga usando
         if (enemigo)
         {
-            float distancia;
 
-            distancia = Vector3.Distance(enemigo.transform.position, transform.position);
-            if (distancia < 9.5f)
+            //Verifico que el enemigo no seas vos
+            if(enemigo != this.gameObject)
             {
-                //Veo a mi objetivo
-                transform.up = enemigo.transform.position - transform.position;
-                auto.Fire();
+                //Establezco una variable para calcular la distancia entre el objetivo y este personaje.
+                float distancia;
+                distancia = Vector3.Distance(enemigo.transform.position, transform.position);
+
+                //Pregunto por si esa distancia está dentro de un rango especifico o no. Si esta dentro, significa que el personaje puede ver su objetivo.
+                if (distancia < 9.5f)
+                {
+
+                    //Veo a mi objetivo
+                    transform.up = enemigo.transform.position - transform.position;
+                    auto.Fire();
+                }
+                else
+                {
+                    //No veo a mi objetivo
+                    transform.up = enemigo.transform.position - transform.position;
+                    auto.Move(transform.up);
+                }
             }
+            //En caso de que el enemigo seas vos, va a entrar a este else y va a cambiar la variable enemigo para que sea nula y busque otro
             else
             {
-                //No veo a mi objetivo
-                transform.up = enemigo.transform.position - transform.position;
-                auto.Move(transform.up);
+                enemigo = null;
             }
+            
+        }
+        else
+        {
+            GameObject[] enemigos = GameObject.FindGameObjectsWithTag("Auto");
+            indice = Random.Range(0, enemigos.Length);
+            enemigo = enemigos[indice];
         }
     }
 }
---
+++ Metal-Retorcido/Assets/Scripts/Consumible.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Consumible : MonoBehaviour
+{
+    public GameObject prefabBala;
+
+    void Update()
+    {
+        //Genero un vector3 para decirle que gire en z para dar la ilusión de un objeto consumible
+        Vector3 giro;
+        giro.x = 0;
+        giro.y = 0;
+        giro.z = 10;
+        transform.Rotate(giro);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //Pregunto si el objeto con el que colisiona tiene el tag auto, si no lo es no me interesa
+        if(collision.gameObject.tag == "Auto")
+        {
+            //Verifico que el consumible sea una bala para que haga su comportamiento
+            if(this.gameObject.tag == "ConsumibleBala")
+            {
+                collision.gameObject.GetComponent<Character>().prefabBala = prefabBala;
+                GameObject.Destroy(this.gameObject);
+            }
+            //Acá verifico si es un consumible de curacion y hago que cumpla su comportamiento
+            else if(this.gameObject.tag == "ConsumibleCuracion")
+            {
+                int life = collision.gameObject.GetComponent<Destroyable>().life;
+                life += 150;
+                if(life > 300)
+                {
+                    life = 300;
+                }
+                GameObject.Destroy(this.gameObject);
+            }
+        }
+    }
+}
